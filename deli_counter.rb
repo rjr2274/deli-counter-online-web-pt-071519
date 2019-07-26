e36ab8c9402adca64c90a89629494da2d1450e0f
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-26 04:05:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    line_string = "The line is currently: "
+    customers = []
+    array.each_with_index do |customer, index|
+       customers << "#{index+1}. #{customer}"
+    end
+    puts line_string + customers.join(" ")
+  end
+end
