3f1439b27206693afbc16d8489d12464bb6651bc
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-26 04:23:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ end
 
 def take_a_number(array, name)
   array << name
-  line_length = array.length + 1
-  puts "Welcome, #{name}.  You are number #{line_length} in line."
+  line_length = array.length
+  puts "Welcome, #{name}. You are number #{line_length} in line."
 end
